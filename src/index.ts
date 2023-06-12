a9b9c6344fe288694b5690cf80841714f6bb520a
Zhiwei Li <65117011+Levix@users.noreply.github.com>
2023-06-12 21:40:52 +0800

    feat: 支持 webpack 向 html 文件注入 js 片段

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,25 @@
 import { createUnplugin } from 'unplugin';
-import type { Options, InjectTypes } from './types';
 import path from 'node:path';
+import type { Options, InjectTypes } from './types';
 
 const formatPath = (path: string) => {
     return path.replace(/\\/g, '/');
 };
 
-export default createUnplugin<Options<InjectTypes>>((options: Options<InjectTypes>) => {
+export const pluginName = 'unplugin-inject-js-snippet';
+
+export default createUnplugin<Options<InjectTypes>>((options: Options<InjectTypes>, meta) => {
+    const { framework } = meta;
     const { inject } = options;
     return {
-        name: 'unplugin-inject-js-snippet',
+        name: pluginName,
         transformInclude(id) {
             const formatId = formatPath(id);
             let shouldTransform = false;
-            if (inject === 'html' && formatId.endsWith('.html')) {
+
+            // WARN: 目前 webpack 不支持这样直接注入 html，得改用 webpack(compiler) 写法，不然会报以下错误
+            // You may need an additional loader to handle the result of these loaders.
+            if (inject === 'html' && formatId.endsWith('.html') && framework !== 'webpack') {
                 const covertTemplates =
                     options.templates?.map(template => formatPath(path.resolve(process.cwd(), template))) ?? [];
                 if (!covertTemplates?.length || covertTemplates.includes(formatId)) {
@@ -32,7 +38,7 @@ export default createUnplugin<Options<InjectTypes>>((options: Options<InjectType
         },
         transform(code) {
             const { injectJs } = options;
-            if (inject === 'html') {
+            if (inject === 'html' && framework !== 'webpack') {
                 const injectScript = `
                     <script>
                         ${injectJs}
--- src/webpack.ts
+++ src/webpack.ts
@@ -1,3 +1,78 @@
-import unplugin from '.';
+import type HtmlWebpackPlugin from 'html-webpack-plugin';
+import type { Compiler } from 'webpack';
+import { sources } from 'webpack';
+import type { Options, InjectTypes } from './types';
+import unplugin, { pluginName } from '.';
 
-export default unplugin.webpack;
+/**
+ * 通过 HtmlWebpackPlugin 获取 html template 模板文件名
+ *
+ * @param compiler Compiler
+ * @returns string[]
+ */
+const getHtmlTemplateFilenames = (compiler: Compiler) => {
+    let htmlTemplateFilenames: string[] = [];
+    const htmlWebpackPluginInfo = compiler.options.plugins.filter(
+        plugin => plugin.constructor.name === 'HtmlWebpackPlugin',
+    ) as HtmlWebpackPlugin[];
+    if (htmlWebpackPluginInfo.length) {
+        const userOptionFilenames = htmlWebpackPluginInfo.map(html => html.userOptions.filename);
+        userOptionFilenames.forEach(userOptionFilename => {
+            const filenameFunction =
+                typeof userOptionFilename === 'function'
+                    ? userOptionFilename
+                    : (entryName: string) => userOptionFilename?.replace(/\[name\]/g, entryName);
+
+            const entryNames = Object.keys(compiler.options.entry);
+            const outputFileNames = Array.from(
+                new Set((entryNames.length ? entryNames : ['main']).map(filenameFunction)),
+            ).filter(item => item !== void 0) as string[];
+            htmlTemplateFilenames = htmlTemplateFilenames.concat(outputFileNames);
+        });
+    }
+    return htmlTemplateFilenames;
+};
+
+export default function (options: Options<InjectTypes>) {
+    const webpackInstance = unplugin.webpack(options);
+    webpackInstance.apply = async (compiler: Compiler) => {
+        compiler.hooks.compilation.tap(pluginName, compilation => {
+            compilation.hooks.processAssets.tap(
+                {
+                    name: pluginName,
+                    additionalAssets: true,
+                },
+                assets => {
+                    const htmlTemplateFilenames = getHtmlTemplateFilenames(compiler);
+                    for (let name in assets) {
+                        if (
+                            name.endsWith('.html') &&
+                            (!htmlTemplateFilenames.length ||
+                                htmlTemplateFilenames.includes(name) ||
+                                options.templates?.includes(name))
+                        ) {
+                            const injectScript = `
+                                <script>
+                                    ${options.injectJs}
+                                </script>
+                            `;
+                            if (!options.templates?.length || options.templates.includes(name)) {
+                                const asset = compilation.getAsset(name);
+                                if (asset) {
+                                    let originalSource = asset.source.source();
+                                    if (originalSource instanceof Buffer) {
+                                        originalSource = originalSource.toString();
+                                    }
+                                    const newSource = originalSource.replace('</head>', `${injectScript}</head>`);
+                                    compilation.updateAsset(name, new sources.ConcatSource(newSource));
+                                }
+                            }
+                        }
+                    }
+                },
+            );
+        });
+    };
+
+    return webpackInstance;
+}
